0e5204edb23c58e8b2c971df6eee3b9cc4c8fe4c
zhubojun <bojun.zhu@foxmail.com>
2020-09-03 15:04:28 +0800

    done

--- src/bin/rthrift_tutorial_client.rs
+++ src/bin/rthrift_tutorial_client.rs
@@ -82,52 +82,66 @@ fn generate_random_buf(buf_size : u64) -> String {
     string_to_send
 }
 
-fn throughput_test_internal(addr: Arc<String>, port: u16, loop_num : u64, req_per_conn: u64, 
-        buf_size : u64) -> thrift::Result<u64> {
+fn throughput_test_internal(vec_queue: Arc<Mutex<VecDeque<SharedServiceSyncClient<ClientInputProtocol, ClientOutputProtocol>>>>,
+    total_num: Arc<Mutex<u64>>, target_num: &u64, buf_size: u64) -> thrift::Result<()> {
     /* First generate packet to send */
     let string_to_send = generate_random_buf(buf_size);
-    //let string_to_send : Rc<String> = Rc::new(string_to_send);
 
-    let mut total_time_in_ms = 0;
-    for _j in 0..loop_num {
-        /* Start client */
-        let mut client = new_client(addr.as_ref(), port)?;
-
-        /* Send request */
-        let time_begin = SystemTime::now();
-        for _i in 0..req_per_conn {
-            let string_to_send = string_to_send.clone();
-            match client.send_packet(string_to_send) {
-                Ok(_res) => {},
-                Err(_e) => {},
+    let mut my_client;
+    let print_num = target_num / 10;
+    loop {
+        loop {
+            if *total_num.lock().unwrap() >= *target_num {
+                return Ok(());
             }
-        }
 
-        let time_end = SystemTime::now();
-        total_time_in_ms += get_duration_in_ms(time_begin, time_end);
+            match vec_queue.lock().unwrap().pop_front() {
+                Some(_client) => {
+                    my_client = _client;
+                    break;
+                },
+                None => {}, 
+            }; 
+            //println!("capacity: {}", vec_queue.lock().unwrap().capacity());
+        }
+        my_client.send_packet(string_to_send.clone())?;
+        vec_queue.lock().unwrap().push_back(my_client);
+
+        let mut lock_arg = total_num.lock().unwrap();
+        if *lock_arg >= *target_num {
+            return Ok(());
+        } else {
+            if *lock_arg % print_num == 0 {
+                println!("arrie here: {}", *lock_arg);
+            }
+            *lock_arg += 1;
+        }
     }
 
     /* Return kB/s here */
-    Ok(
-        ((loop_num * req_per_conn * buf_size / 1024) as f64 / (total_time_in_ms as f64 / 1000 as f64)) as u64
-    )
+    Ok(())
 }
 
-fn run_throughput_test(addr: Arc<String>, port: u16, loop_num: u64, thread_num: u64, req_per_conn: u64, 
+fn run_throughput_test(thread_num: u64, target_num: u64, conn_num: u64, 
     buf_size : u64) -> thrift::Result<()> {
     let mut handler_vec: Vec<JoinHandle<()>> = Vec::new();
-    let thp_array : Arc<Mutex<Vec<u64>>> = Arc::new(Mutex::new(Vec::new()));
-	
-	//let core_ids = core_affinity::get_core_ids().unwrap();
-	let cur_core_id = core_affinity::CoreId{ id : thread_num as usize % 32};
-	core_affinity::set_for_current(cur_core_id);
+    
+    let connect_vec = Arc::new(Mutex::new(VecDeque::new()));
+    /* Create connections */
+    for _i in 0..conn_num {
+        let client = new_client(SERVER_ADDR, PORT)?;
+        connect_vec.lock().unwrap().push_back(client);
+    }
+
     /* Spawn threads */
+    let total_num = Arc::new(Mutex::new(0));
+    let begin_time = SystemTime::now();
     for _i in 0..thread_num {
-        let addr = addr.clone();
-        let thp_array = thp_array.clone();
+        let total_num = total_num.clone();
+        let connect_vec = connect_vec.clone();
         let handler = thread::spawn( move || {
-            match throughput_test_internal(addr, port, loop_num, req_per_conn, buf_size) {
-                Ok(thp) => { thp_array.lock().unwrap().push(thp); },
+            match throughput_test_internal(connect_vec, total_num, &target_num, buf_size) {
+                Ok(_) => { },
                 Err(_e) => { println!("[gbd] run_latency_test error here 1!"); },
             };
         });
@@ -138,54 +152,85 @@ fn run_throughput_test(addr: Arc<String>, port: u16, loop_num: u64, thread_num: 
     for handler in handler_vec {
         handler.join().unwrap();
     }
+    let end_time = SystemTime::now();
 
     /* Sort the array */
-    let mut total_thp = 0;
-    for thp in thp_array.lock().unwrap().iter() {
-        total_thp += thp;
-    }
+    let total_time_in_ms = get_duration_in_ms(begin_time, end_time);
+    let total_thp = (target_num * buf_size / 1024) as f64 / 
+            (total_time_in_ms as f64 / 1000 as f64);
 
-    println!("Throughput: {}kB/s", total_thp / thread_num);
+    println!("Throughput: {}kB/s", total_thp);
     
     Ok(())
 }
 
-fn latency_test_internal(req_per_conn: u64, 
-    buf_size : u64) -> thrift::Result<Vec<u64>> {
+fn latency_test_internal(vec_queue: Arc<Mutex<VecDeque<SharedServiceSyncClient<ClientInputProtocol, ClientOutputProtocol>>>>,
+    total_num: Arc<Mutex<u64>>, target_num: &u64, buf_size: u64) -> thrift::Result<Vec<u64>> {
     /* First generate packet to send */
     let string_to_send = generate_random_buf(buf_size);
-    
+
     /* Generate array to store the time distribution */
     let mut time_array : Vec<u64> = Vec::new();
-    for _i in 0..req_per_conn { time_array.push(0); }
 
-    /* Start client and send request */
-    let mut client = new_client(SERVER_ADDR, PORT)?;
+    let mut my_client;
+    let print_num = target_num / 10;
+    loop {
+        loop {
+            if *total_num.lock().unwrap() >= *target_num {
+                return Ok(time_array);
+            }
 
-    for _i in 0..req_per_conn {
-        let time_begin = SystemTime::now();
-        match client.send_packet(string_to_send.clone()) {
-            Ok(_res) => {},
-            Err(_e) => {},
+            match vec_queue.lock().unwrap().pop_front() {
+                Some(_client) => {
+                    my_client = _client;
+                    break;
+                },
+                None => {}, 
+            }; 
+            //println!("capacity: {}", vec_queue.lock().unwrap().capacity());
         }
+        let time_begin = SystemTime::now();
+        my_client.send_packet(string_to_send.clone())?;
         let time_end = SystemTime::now();
-        time_array[_i as usize] = get_duration_in_ns(time_begin, time_end);
+
+        time_array.push(get_duration_in_ns(time_begin, time_end));
+        vec_queue.lock().unwrap().push_back(my_client);
+
+        let mut lock_arg = total_num.lock().unwrap();
+        if *lock_arg >= *target_num {
+            return Ok(time_array);
+        } else {
+            if *lock_arg % print_num == 0 {
+                println!("arrie here: {}", *lock_arg);
+            }
+            *lock_arg += 1;
+        }
     }
     
     //println!("[gbd] time array 0: {}", time_array[0]);
     Ok(time_array)
 }
 
-fn run_latency_test(thread_num: u64, req_per_conn: u64, 
-    buf_size : u64) -> std::io::Result<()> {
-    let mut handler_vec: Vec<JoinHandle<()>> = Vec::new();
-    let time_array: Arc<Mutex<Vec<u64>>> = Arc::new(Mutex::new(Vec::new()));
+fn run_latency_test(thread_num: u64, req_num: u64, conn_num: u64,
+    buf_size : u64) -> thrift::Result<()> {
+    let connect_vec = Arc::new(Mutex::new(VecDeque::new()));
+    /* Create connections */
+    for _i in 0..conn_num {
+        let client = new_client(SERVER_ADDR, PORT)?;
+        connect_vec.lock().unwrap().push_back(client);
+    }
 
     /* Spawn threads */
+    let mut handler_vec: Vec<JoinHandle<()>> = Vec::new();
+    let time_array: Arc<Mutex<Vec<u64>>> = Arc::new(Mutex::new(Vec::new()));
+    let total_num = Arc::new(Mutex::new(0));
+
     for _i in 0..thread_num {
+        let connect_vec = connect_vec.clone();
         let time_array = time_array.clone();
+        let total_num = total_num.clone();
         let handler = thread::spawn( move || {
-            match latency_test_internal(req_per_conn, buf_size) {
+            match latency_test_internal(connect_vec, total_num, &req_num, buf_size) {
                 Ok(ref mut _vec_ret) => { time_array.lock().unwrap().append(_vec_ret); },
                 Err(_e) => { println!("[gbd] run_latency_test error here 1!"); },
             };
@@ -204,7 +249,7 @@ fn run_latency_test(thread_num: u64, req_per_conn: u64,
     /* Open file and output the result */
     let mut total_time = 0;
     let path_str = format!("latency_{}t_{}_{}kB_in_ns", 
-            thread_num, req_per_conn, buf_size / 1024);
+            thread_num, req_num, buf_size / 1024);
     
     let mut file = File::create(&path_str)?;
     for time in time_array.lock().unwrap().iter() {
@@ -213,8 +258,9 @@ fn run_latency_test(thread_num: u64, req_per_conn: u64,
     }
 
     /* Write the Average Time */
-    file.write_all(format!("average time: {}", 
-       total_time as f64 / (req_per_conn * thread_num) as f64).as_bytes())?;
+    let average_time = (total_time as f64 / req_num as f64) as u64;
+    file.write_all(format!("average time: {} in ns", 
+                        average_time).as_bytes())?;
     println!("[gbd] arrive here after open a file");
 
     Ok(())
@@ -307,8 +353,8 @@ fn main() {
         (about: "Thrift Rust tutorial client")
         (@arg host: --host +takes_value "host on which the tutorial server listens")
         (@arg port: --port +takes_value "port on which the tutorial server listens")
-        (@arg iter: --iter +takes_value "Iteration Numbers")
         (@arg thread: --thread +takes_value "Thread Numbers")
+        (@arg conn_num: --conn +takes_value "Connection Numbers")
         (@arg reqnum: --reqnum +takes_value "Request Numbers")
         (@arg bufsize: --bufsize +takes_value "Buffer Size in kB")
         (@arg option: --option +takes_value "Option")
@@ -316,24 +362,22 @@ fn main() {
     let matches = options.get_matches();
 
      // get any passed-in args or the defaults
-    let host = matches.value_of("host").unwrap_or("127.0.0.1");
-    let port = value_t!(matches, "port", u16).unwrap_or(11235);
     let loop_num = value_t!(matches, "iter", u64).unwrap_or(5000);
-    let thread_num = value_t!(matches, "thread", u64).unwrap_or(12);
-    let req_per_conn = value_t!(matches, "reqnum", u64).unwrap_or(5000);
+    let thread_num = value_t!(matches, "thread", u64).unwrap_or(30);
+    let req_num = value_t!(matches, "reqnum", u64).unwrap_or(50000);
     let buf_size_in_kb = value_t!(matches, "bufsize", u64).unwrap_or(1);
     let option = value_t!(matches, "option", u64).unwrap_or(0);
+    let conn_num = value_t!(matches, "conn", u64).unwrap_or(10);
 
-    println!("Client configuration: IP: {}:{}, iter: {} thread_num: {} req_per_conn {} buf_size per req {} kB option: {}",
-                SERVER_ADDR, PORT, loop_num, thread_num, req_per_conn, buf_size_in_kb,
+    println!("Client configuration: IP: {}:{}, thread_num: {} req_num {} buf_size per req {} kB option: {}",
+                SERVER_ADDR, PORT, thread_num, req_num, buf_size_in_kb,
                 option);
-    
-    let host_arc = Arc::new(host.to_string());  
+     
     let buf_size = buf_size_in_kb * 1024;
     match option {
         /* Run qps test */
         0 => {
-            match run_qps_test(thread_num, 100000, 10) {
+            match run_qps_test(thread_num, req_num, conn_num) {
                 Ok(_ok) => {},
                 Err(_err) => { println!("run_qps_test err: {:?}", _err); },
             };
@@ -341,7 +385,7 @@ fn main() {
 
         /* Run throughput test */
         1 => {
-            match run_throughput_test(host_arc, port, loop_num, thread_num, req_per_conn, buf_size) {
+            match run_throughput_test(thread_num, req_num, conn_num, buf_size) {
                 Ok(_ok) => {},
                 Err(_err) => { println!("run_throughput_test err: {:?}", _err); },
             };
@@ -349,7 +393,7 @@ fn main() {
 
         /* Run latency test */
         2 => {
-            match run_latency_test(thread_num, req_per_conn, buf_size) {
+            match run_latency_test(thread_num, req_num, conn_num, buf_size) {
                 Ok(_ok) => {},
                 Err(_err) => { println!("run_latency_test err: {:?}", _err); }
             };
