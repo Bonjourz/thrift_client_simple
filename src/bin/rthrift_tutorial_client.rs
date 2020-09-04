aa9c3a4a79ce6ab9a59528125c4cf33f49ac628f
zhubojun <bojun.zhu@foxmail.com>
2020-09-04 11:38:15 +0800

    add async

--- src/bin/rthrift_tutorial_client.rs
+++ src/bin/rthrift_tutorial_client.rs
@@ -20,12 +20,9 @@ extern crate clap;
 
 extern crate rthrift as thrift;
 extern crate rthrift_tutorial;
-use futures::executor::block_on;
-use std::error::Error;
 extern crate tokio;
 use tokio::runtime::Runtime;
 use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
-use tokio::io;
 
 use std::sync::{Arc, Mutex};
 
@@ -33,13 +30,14 @@ use tokio::task::JoinHandle;
 use std::collections::VecDeque;
 use ringbuf::{RingBuffer, Consumer, Producer};
 
-use std::rc::Rc;
-use std::cell::RefCell;
 use tokio::net::{TcpStream};
 
-use tokio::sync::mpsc;
+use rand::prelude::*;
+use std::mem;
+use std::fs::File;
+use std::io::prelude::*;
 
-use std::time::{SystemTime, Duration};
+use std::time::{Instant, Duration};
 
 use thrift::protocol::{TAsyncBinaryInputProtocol, TAsyncBinaryOutputProtocol};
 use thrift::transport::{TFramedReadTransport, TFramedWriteTransport, 
@@ -54,36 +52,33 @@ use thrift::transport::{
 use rthrift_tutorial::shared::{TSharedServiceSyncClient, SharedServiceSyncClient};
 
 use pprof;
-use std::fs::File;
 
-// fn main() {
-//     match run() {
-//         Ok(()) => println!("tutorial client ran successfully"),
-//         Err(e) => {
-//             println!("tutorial client failed with error {:?}", e);
-//             std::process::exit(1);
-//         }
-//     }
-// }
-
-fn get_duration(start_time : SystemTime, end_time: SystemTime) -> Duration {
-    let duration = end_time.duration_since(start_time);
-    let mut duration_time = Duration::from_secs(0);
-    match duration {
-        Ok(_d) => {duration_time = _d;},
-        _ => { println!("[gbd] get duration error!"); },
-    }
-    duration_time
+fn get_duration(start_time : Instant, end_time: Instant) -> Duration {
+    end_time.duration_since(start_time)
 }
 
-fn get_duration_in_ms(start_time : SystemTime, end_time: SystemTime) -> u64 {
+fn get_duration_in_ms(start_time : Instant, end_time: Instant) -> u64 {
     let res = get_duration(start_time, end_time).as_millis() as u64;
+    //println!("[gbd] begin: {:?} end: {:?} duration: {}", start_time, end_time, res);
     res
 }
 
-fn get_duration_in_ns(start_time : SystemTime, end_time: SystemTime) -> u64 {
+fn get_duration_in_ns(start_time : Instant, end_time: Instant) -> u64 {
     let res = get_duration(start_time, end_time).as_nanos() as u64;
+    //println!("[gbd] begin: {:?} end: {:?} duration: {}", start_time, end_time, res);
     res
+}
+
+fn generate_random_buf(buf_size : u64) -> String {
+    let mut string_to_send = String::from("");
+    let mut rng = rand::thread_rng();
+    let mut rand_num : u64;
+    for _i in 0..(buf_size / (mem::size_of::<u64>() as u64)) {
+        rand_num = rng.gen(); // Random Number
+        string_to_send.push_str(&rand_num.to_string());
+    }
+
+    string_to_send
 }
 
 
@@ -91,66 +86,6 @@ type ClientInputProtocol = TAsyncBinaryInputProtocol<TAsyncBufferedReadTransport
 type ClientOutputProtocol = TAsyncBinaryOutputProtocol<TAsyncBufferedWriteTransport<OwnedWriteHalf>>;
 
 const SERVER_TARGET : &'static str = "127.0.0.1:11235";
-
-async fn async_run_client_one(host : String, port: u16,
-    args1: i32) -> thrift::Result<i32> {
-    let mut client = new_client().await?;
-
-     // alright!
-    // let's start making some calls
-
-    // let's start with a ping; the server should respond
-
-    let _gbd_val = 1000;
-    match client.add(1, 2).await {
-        Ok(_val) => { println!("get the result: {}", _gbd_val); }
-        Err(_e) => { println!("error string: {}", _e); }
-    };
-    println!("val: {}", _gbd_val);
-    Ok(1)
-}
-
-async fn run_all_sync(/*res_ref1: Rc<RefCell<i32>>, res_ref2: Rc<RefCell<i32>>*/)
-    -> (Option<i32>, Option<i32>) {
-    
-    // let res1 = tokio::spawn(async move {
-    //     let host1 = String::from("127.0.0.1");
-    //     async_run_client_one(host1, 11235, 2).await
-    // });
-    let host1 = String::from("127.0.0.1");
-    async_run_client_one(host1, 11235, 2).await;
-
-    // let mut res_wrap;
-    // let res2 = tokio::spawn(async move {
-    //     let host2  = String::from("127.0.0.1");
-    //     async_run_client_one(host2, 11235, 3).await
-    // });
-
-    // let result_1 = Rc::clone(&res_ref1);
-    // let result_2 = Rc::clone(&res_ref2);
-
-    // let res_1 : i32 = res1;
-    // let res_2 : i32 = res2;
-    // let (_first, _second) = tokio::join!(
-    //     res1, res2
-    // );
-    //let _first = tokio::join!(res1);
-
-    //let test : thrift::Result<i32> = Ok(_first);
-    let mut final_ret : i32 = 0;
-    // match _first {
-    //     // Ok(thrift_result) => final_ret = thrift_result,
-    //     Ok(thrift_result) => {
-    //         match thrift_result {
-    //             Ok(_val) => final_ret = _val,
-    //             Err(_e) => println!("error"),
-    //         }
-    //     },
-    //     Err(e) => println!("tutorial client failed with error {:?}", e),
-    // }
-    println!("[gbd] arrive here");
-    (Some(final_ret), Some(2))
-}
 
 async fn test_qps(ring_prod_arc: Arc<Mutex<Producer<SharedServiceSyncClient<ClientInputProtocol, ClientOutputProtocol>>>>,
     ring_cons_arc: Arc<Mutex<Consumer<SharedServiceSyncClient<ClientInputProtocol, ClientOutputProtocol>>>>,
@@ -173,9 +108,10 @@ async fn test_qps(ring_prod_arc: Arc<Mutex<Producer<SharedServiceSyncClient<Clie
             }; 
         }
 
-        let result = my_client.add(1, 2).await?;
-        assert_eq!(result, 3);
-       // println!("client arrive here 1, thread_idx: {}", t_idx);
+        my_client.send_empty().await?;
+        // let result = my_client.add(1, 2).await?;
+        // assert_eq!(result, 3);
+        // println!("client arrive here 1, thread_idx: {}", t_idx);
         ring_prod_arc.lock().unwrap().push(my_client);
         //println!("client arrive here 2");
 
@@ -208,7 +144,7 @@ async fn run_all_async_qps(thread_num: u64, target_num: u64, conn_num: u64) -> t
     let initial_total = Arc::new(Mutex::new(0));
     let ring_prod_arc = Arc::new(Mutex::new(prod));
     let ring_cons_arc = Arc::new(Mutex::new(cons));
-    let time_begin = SystemTime::now();
+    let time_begin = Instant::now();
     println!("thread num: {}", thread_num);
     for _i in 0..thread_num {
         let initial_total = initial_total.clone();
@@ -227,10 +163,198 @@ async fn run_all_async_qps(thread_num: u64, target_num: u64, conn_num: u64) -> t
         handle.await;
     }
 
-    let time_end = SystemTime::now();
+    let time_end = Instant::now();
     let time_in_ms = get_duration_in_ms(time_begin, time_end);
     println!("[gbd] the total: {}", *initial_total.lock().unwrap());
     println!("{} Req/s", (target_num) as f64 / (time_in_ms as f64 / 1000 as f64));
+
+    Ok(())
+}
+
+async fn latency_test_internal(vec_queue: Arc<Mutex<VecDeque<SharedServiceSyncClient<ClientInputProtocol, ClientOutputProtocol>>>>,
+    total_num: Arc<Mutex<u64>>, target_num: &u64, buf_size: u64) -> thrift::Result<Vec<u64>> {
+        /* First generate packet to send */
+    let string_to_send = generate_random_buf(buf_size);
+
+    /* Generate array to store the time distribution */
+    let mut time_array : Vec<u64> = Vec::new();
+
+    let mut my_client;
+    let print_num = target_num / 10;
+    loop {
+        loop {
+            if *total_num.lock().unwrap() >= *target_num {
+                return Ok(time_array);
+            }
+
+            match vec_queue.lock().unwrap().pop_front() {
+                Some(_client) => {
+                    my_client = _client;
+                    break;
+                },
+                None => {}, 
+            }; 
+            //println!("capacity: {}", vec_queue.lock().unwrap().capacity());
+        }
+        let time_begin = Instant::now();
+        my_client.send_packet(string_to_send.clone()).await?;
+        let time_end = Instant::now();
+
+        time_array.push(get_duration_in_ns(time_begin, time_end));
+        vec_queue.lock().unwrap().push_back(my_client);
+
+        let mut lock_arg = total_num.lock().unwrap();
+        if *lock_arg >= *target_num {
+            return Ok(time_array);
+        } else {
+            if *lock_arg % print_num == 0 {
+                println!("arrie here: {}", *lock_arg);
+            }
+            *lock_arg += 1;
+        }
+    }
+    
+    //println!("[gbd] time array 0: {}", time_array[0]);
+    Ok(time_array)
+}
+
+async fn run_all_async_latency(thread_num: u64, req_num: u64, conn_num: u64,
+    buf_size : u64) -> thrift::Result<()> {
+    let connect_vec = Arc::new(Mutex::new(VecDeque::new()));
+    /* Create connections */
+    for _i in 0..conn_num {
+        let client = new_client().await?;
+        connect_vec.lock().unwrap().push_back(client);
+    }
+
+    /* Spawn threads */
+    let mut handler_vec: Vec<JoinHandle<()>> = Vec::new();
+    let time_array: Arc<Mutex<Vec<u64>>> = Arc::new(Mutex::new(Vec::new()));
+    let total_num = Arc::new(Mutex::new(0));
+
+    for _i in 0..thread_num {
+        let connect_vec = connect_vec.clone();
+        let time_array = time_array.clone();
+        let total_num = total_num.clone();
+        let handler = tokio::spawn(async move {
+            match latency_test_internal(connect_vec, total_num, &req_num, buf_size).await {
+                Ok(ref mut _vec_ret) => { time_array.lock().unwrap().append(_vec_ret); },
+                Err(_e) => { println!("[gbd] error"); }
+            };
+        });
+        handler_vec.push(handler);
+    }
+
+     /* Join threads */
+    for handler in handler_vec {
+        handler.await;
+    }
+
+    /* Sort the array */
+    time_array.lock().unwrap().sort();
+
+    /* Open file and output the result */
+    let mut total_time = 0;
+    let path_str = format!("latency_{}t_{}_{}kB_in_ns", 
+            thread_num, req_num, buf_size / 1024);
+    
+    let mut file : std::fs::File = File::create(&path_str)?;
+    for time in time_array.lock().unwrap().iter() {
+        total_time += time;
+        file.write_all(format!("{}\n", time).as_bytes())?;
+    }
+
+    /* Write the Average Time */
+    let average_time = (total_time as f64 / req_num as f64) as u64;
+    file.write_all(format!("average time: {} in ns", 
+                    average_time).as_bytes())?;
+    println!("[gbd] arrive here after open a file");
+
+
+
+    Ok(())
+}
+
+async fn throughput_test_internal(vec_queue: Arc<Mutex<VecDeque<SharedServiceSyncClient<ClientInputProtocol, ClientOutputProtocol>>>>,
+    total_num: Arc<Mutex<u64>>, target_num: &u64, buf_size: u64) -> thrift::Result<()> {
+    /* First generate packet to send */
+    let string_to_send = generate_random_buf(buf_size);
+
+    let mut my_client;
+    let print_num = target_num / 10;
+    loop {
+        loop {
+            if *total_num.lock().unwrap() >= *target_num {
+                return Ok(());
+            }
+
+            match vec_queue.lock().unwrap().pop_front() {
+                Some(_client) => {
+                    my_client = _client;
+                    break;
+                },
+                None => {}, 
+            }; 
+            //println!("capacity: {}", vec_queue.lock().unwrap().capacity());
+        }
+        my_client.send_packet(string_to_send.clone()).await?;
+        vec_queue.lock().unwrap().push_back(my_client);
+
+        let mut lock_arg = total_num.lock().unwrap();
+        if *lock_arg >= *target_num {
+            return Ok(());
+        } else {
+            if *lock_arg % print_num == 0 {
+                println!("arrie here: {}", *lock_arg);
+            }
+            *lock_arg += 1;
+        }
+    }
+
+    /* Return kB/s here */
+    Ok(())
+}
+
+async fn run_all_async_throughput(thread_num: u64, target_num: u64, conn_num: u64, 
+    buf_size : u64) -> thrift::Result<()> {
+    let mut handler_vec: Vec<JoinHandle<()>> = Vec::new();
+    
+    let connect_vec = Arc::new(Mutex::new(VecDeque::new()));
+        /* Create connections */
+    for _i in 0..conn_num {
+        let client = new_client().await?;
+        connect_vec.lock().unwrap().push_back(client);
+    }
+
+    /* Spawn threads */
+    let total_num = Arc::new(Mutex::new(0));
+    let begin_time = Instant::now();
+
+    for _i in 0..thread_num {
+        let total_num = total_num.clone();
+        let connect_vec = connect_vec.clone();
+        
+        let handler = tokio::spawn(async move {
+            match throughput_test_internal(connect_vec, total_num, &target_num, buf_size).await {
+                Ok(_) => { },
+                Err(_e) => { println!("[gbd] run_throughput_test error here 1!"); },
+            };
+        });
+        handler_vec.push(handler);
+    }
+
+     /* Join threads */
+    for handler in handler_vec {
+        handler.await;
+    }
+    let end_time = Instant::now();
+
+    /* Sort the array */
+    let total_time_in_ms = get_duration_in_ms(begin_time, end_time);
+    let total_thp = (target_num * buf_size / 1024) as f64 / 
+            (total_time_in_ms as f64 / 1000 as f64);
+
+    println!("Throughput: {}kB/s", total_thp);
 
     Ok(())
 }
@@ -252,9 +376,9 @@ fn main() {
 
     let loop_num = value_t!(matches, "iter", u64).unwrap_or(5000);
     let thread_num = value_t!(matches, "thread", u64).unwrap_or(1000);
-    let req_num = value_t!(matches, "reqnum", u64).unwrap_or(50000);
+    let req_num = value_t!(matches, "reqnum", u64).unwrap_or(5000000);
     let buf_size_in_kb = value_t!(matches, "bufsize", u64).unwrap_or(1);
-    let option = value_t!(matches, "option", u64).unwrap_or(0);
+    let option = value_t!(matches, "option", u64).unwrap_or(1);
     let conn_num = value_t!(matches, "conn", u64).unwrap_or(1000);
 
     println!("Client configuration: IP: {}, thread_num: {} req_num {} buf_size per req {} kB option: {}",
@@ -264,7 +388,7 @@ fn main() {
     let buf_size = buf_size_in_kb * 1024;
 
     let mut rt = Runtime::new().unwrap();
-    // let guard = pprof::ProfilerGuard::new(100).unwrap();
+    let guard = pprof::ProfilerGuard::new(100).unwrap();
 
     match option {
         /* Run qps test */
@@ -276,21 +400,20 @@ fn main() {
         },
 
         /* Run throughput test */
-        /*
         1 => {
-            match run_throughput_test(thread_num, req_num, conn_num, buf_size) {
+            match rt.block_on(run_all_async_throughput(thread_num, req_num, conn_num, buf_size)) {
                 Ok(_ok) => {},
                 Err(_err) => { println!("run_throughput_test err: {:?}", _err); },
             };
-        }, */
+        },
 
         /* Run latency test */
-        /*2 => {
-            match run_latency_test(thread_num, req_num, conn_num, buf_size) {
+        2 => {
+            match rt.block_on(run_all_async_latency(thread_num, req_num, conn_num, buf_size)) {
                 Ok(_ok) => {},
                 Err(_err) => { println!("run_latency_test err: {:?}", _err); }
             };
-        }, */
+        },
 
         _op => { println!("Error: Invalide option: {}", _op); },
     }
@@ -300,15 +423,15 @@ fn main() {
 
     println!("arrive here after rt enter");
 
-    // match guard.report().build() {
-    //     Ok(report) => {
-    //         let file = File::create("flamegraph.svg").unwrap();
-    //         report.flamegraph(file).unwrap();
+    match guard.report().build() {
+        Ok(report) => {
+            let file = File::create("flamegraph.svg").unwrap();
+            report.flamegraph(file).unwrap();
 
-    //         println!("report: {}", &report);
-    //     }
-    //     Err(_) => {}
-    // };
+            //println!("report: {}", &report);
+        }
+        Err(_) => {}
+    };
 
     /* Barrier here */
 
@@ -320,10 +443,6 @@ fn main() {
     // let handle = tokio::spawn(async move {
     //     run_all_sync
     // }).await.unwrap();
-
-    // 
-    // tokio_compat::run_std(run_all_sync());
-    //tokio::
 }
 
 // type ClientInputProtocol = TBinaryInputProtocol<TFramedReadTransport<ReadHalf<TTcpChannel>>>;
